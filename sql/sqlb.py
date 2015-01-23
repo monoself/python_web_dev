1db202873f4e4ed95e33cf1fa6c683b832bef93b
Xiaoyang Zhang <averto@gmail.com>
2015-01-23 22:04:47 +0800

    SQL database operation executemany()

--- sql/sqlb.py
+++ sql/sqlb.py
@@ -5,11 +5,14 @@ import sqlite3
 conn = sqlite3.connect("new.db")
 cursor = conn.cursor()
 
-# insert data
-cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+try:
+    # insert data
+    cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+    cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
 
-# commit the changes: INSERT, UPDATE, DELETE
-conn.commit()
+    # commit the changes: INSERT, UPDATE, DELETE
+    conn.commit()
+except sqlite3.OperationalError:
+    print("Try again...")
 
 conn.close()
--- sql/sqlc.py
+++ sql/sqlc.py
@@ -1,17 +1,26 @@
-# executemany() metho
+# executemany() method
 
-import sqlite3
+import sqlite3, csv
 
 with sqlite3.connect("new.db") as connection:
-	c = connection.cursor()
+    c = connection.cursor()
 
-	# insert multiple records using a tuple
-	cities = [
-	('Boston', 'MA', 60000),
-	('Chicago', 'IL', 2700000),
-	('Houston', 'TX', 2100000),
-	('Phoenix', 'AZ', 1500000)
-	]
+    # insert multiple records using a tuple
+    cities = [
+    ('Boston', 'MA', 60000),
+    ('Chicago', 'IL', 2700000),
+    ('Houston', 'TX', 2100000),
+    ('Phoenix', 'AZ', 1500000)
+    ]
 
-	# insert data into database
-	c.executemany('INSERT INTO population VALUES(?, ?, ?)', cities) # ? as parameterized statements
+    # insert data into database
+    c.executemany('INSERT INTO population VALUES(?, ?, ?)', cities) # ? as parameterized statements
+
+    # open the csv file and assign it to a variable
+    employees = csv.reader(open("employees.csv", "rU"))
+    
+    # create a new table called employees
+    c.execute("CREATE TABLE employees(firstname TEXT, lastname TEXT)")
+
+    # insert data into table
+    c.executemany("INSERT INTO employees(firstname, lastname) values (?, ?)", employees)
